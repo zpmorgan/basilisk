e503987ae978713b120e1ffafabba9ca475a8143
zpmorgan@SPAN.UNCG.EDU <zpmorgan@SPAN.UNCG.EDU@035b029e-814f-4f2e-88e1-34f0099224d4>
2009-02-14 04:54:01 +0000

    hyperlinks on empty board cells
    git-svn-id: https://span.uncg.edu/svn/basilisk@13 035b029e-814f-4f2e-88e1-34f0099224d4

--- lib/basilisk/Controller/Game.pm
+++ lib/basilisk/Controller/Game.pm
@@ -6,11 +6,16 @@ use parent 'Catalyst::Controller';
 use basilisk::Util;
 
 
+__PACKAGE__->config->{namespace} = '';
+
 # /game/14?foo=bar
 sub game : Global {
    my ( $self, $c ) = @_;
    #extract game id from path
    my ($gameid) = $c->req->path =~ m|game/(\d*)|;
+   my $action = $c->req->param('action');
+   #$c->stash->{msg} = 'action is '.$action.',co is '.$c->req->param('co');
+   
    $c->stash->{gameid} = $gameid;
    $c->stash->{game} = $c->model('DB::Game')->search( 'id' => $c->stash->{gameid})->next;
    unless ($gameid ){
@@ -24,7 +29,7 @@ sub game : Global {
    $c->stash->{players_html} = display_players_html($c);
    $c->stash->{last_move} = $c->stash->{game}->last_move;
    $c->stash->{template} = 'game.tt';
-   $c->stash->{title}= "Game ".$c->stash->{gameid}.", move ".$c->stash->{last};
+   $c->stash->{title}= "Game ".$c->stash->{gameid}.", move ".$c->stash->{last_move};
    $c->session->{num}++;
    $c->stash->{num} = $c->session->{'num'};
    $c->stash->{board_html} = render_board_html($c,$gameid);
@@ -35,26 +40,6 @@ sub game : Global {
    #die $page;
 }
 
-sub select_g_file{ #default board
-   my ($stone, $size, $row, $col) = @_;
-   return 'b.gif' if $stone == 1;
-   return 'w.gif' if $stone == 2;
-   #$stone==0 -- so it's an empty intersection
-   #several empties to choose from:
-   if ($row == 0){
-      return 'ul.gif' if $col == 0;
-      return 'ur.gif' if $col == $size-1;
-      return 'u.gif' ;
-   }
-   if ($row == $size-1){
-      return 'dl.gif' if $col == 0;
-      return 'dr.gif' if $col == $size-1;
-      return 'd.gif' ;
-   }
-   return 'el.gif' if $col == 0;
-   return 'er.gif' if $col == $size-1;
-   return 'e.gif'
-}
 
 sub display_players_html{
    my ($c) = @_;
@@ -85,6 +70,7 @@ sub display_players_html{
    return join "\n", @lines;
 }
 
+
 sub render_board_html{
    my ($c) = @_;
    my $size = $c->stash->{game}->size;
@@ -94,20 +80,55 @@ sub render_board_html{
    my $pos = $c->stash->{game}->current_position;
    my $board = Util::unpack_position($pos, $size);
    
-   #render board position as a table
+   #render this board as a html table
    push @lines, q|<table  class="Goban" style="background-image: url(/g/wood.gif);">|;
+   #todo: coordinate letters
    for my $rownum (0..$size-1){
       push @lines, q|<tr>|;
-      for my $colnum (0..$size-1){
-         push @lines, q|<td class="brdx"> <img class="brdx" src="/g/| . 
-            select_g_file ($board->[$rownum]->[$colnum], $size, $rownum, $colnum)
-            . q|" /> </td>|;
+      #todo: coordinate number column
+      for my $colnum (0..$size-1){ #form one intersection. if empty, it's a link
+         my $stone = $board->[$rownum]->[$colnum]; #0 if empty, 1 b, 2 w
+         my $image = select_g_file ($stone, $size, $rownum, $colnum);
+         $image = "<img class='brdx' src='/g/$image'>";
+         if ($stone==0){ #empty, so clickable
+            my $url = "/game/".$c->stash->{gameid} . "?action=move&co=" . $rownum .'.'.$colnum;
+            $image = "<a href='$url'>$image</a>";
+         }
+         my $cell = q|<td class="brdx"> |;
+         $cell .= $image;
+         $cell .= q|</td>|;
+         push @lines, $cell;
+         #push @lines, q|<td class="brdx"> <img class="brdx" src="/g/| . 
+         #   select_g_file ($board->[$rownum]->[$colnum], $size, $rownum, $colnum)
+         #   . q|" /> </td>|;
       }
+      #todo: coordinate number column
       push @lines, q|</tr>|;
    }
-   #push @lines, join "<br>\n",map{join" ",@$_} @$board;
+   #todo: coordinate letters
    push @lines, '</table>';
    return join "\n", @lines;
+}
+
+sub select_g_file{ #default board
+   my ($stone, $size, $row, $col) = @_;
+   return 'b.gif' if $stone == 1;
+   return 'w.gif' if $stone == 2;
+   #$stone==0 -- so it's an empty intersection
+   #several empties to choose from:
+   if ($row == 0){
+      return 'ul.gif' if $col == 0;
+      return 'ur.gif' if $col == $size-1;
+      return 'u.gif' ;
+   }
+   if ($row == $size-1){
+      return 'dl.gif' if $col == 0;
+      return 'dr.gif' if $col == $size-1;
+      return 'd.gif' ;
+   }
+   return 'el.gif' if $col == 0;
+   return 'er.gif' if $col == $size-1;
+   return 'e.gif'
 }
 
 sub get_game_board{
