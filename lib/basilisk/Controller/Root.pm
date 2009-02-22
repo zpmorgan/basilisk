8dc874e21f79470b96c3ae07a94fde1563909015
zpmorgan@SPAN.UNCG.EDU <zpmorgan@SPAN.UNCG.EDU@035b029e-814f-4f2e-88e1-34f0099224d4>
2009-02-22 05:55:18 +0000

    img_base absorbs /g
    git-svn-id: https://span.uncg.edu/svn/basilisk@52 035b029e-814f-4f2e-88e1-34f0099224d4

--- lib/basilisk/Controller/Root.pm
+++ lib/basilisk/Controller/Root.pm
@@ -59,11 +59,12 @@ sub end : ActionClass('RenderView') {
    my ( $self, $c ) = @_;
    #set some tt vars for header
    my ($url_base) = $c->request->base =~ m|http://[^/]*(/.*)/$|;
+   my $img_base = $url_base . '/g';
    $c->stash->{url_base} = $url_base;
-   $c->stash->{img_base} = $url_base;
+   $c->stash->{img_base} = $img_base;
    if ($c->stash->{message}){ #TT can't do this at runtime?
       $c->stash->{message} =~ s/\[\%\s?url_base\s?\%\]/$url_base/;
-      $c->stash->{message} =~ s/\[\%\s?img_base\s?\%\]/$url_base/;
+      $c->stash->{message} =~ s/\[\%\s?img_base\s?\%\]/$img_base/;
    }
    $c->stash->{logged_in} = $c->session->{logged_in} ? 1 : 0;
    #$c->stash->{name} seems to be basilisk, and unchangeable.
