b48be9279cf1a8c613acfc86b9a2b83cb3235598
Zach Morgan <zpmorgan@gmail.com>
2009-03-17 14:27:09 -0400

    part of a new rulemap subsystem using moose roles, which will modify capture/placement methods

---
+++ lib/basilisk/Rulemap/Atom.pm
@@ -0,0 +1,20 @@
+package basilisk::Rulemap::Atom;
+#rulemap modifier
+use Mouse::Role;
+
+requires ('remove_caps');
+
+#after 'remove_caps' => sub{
+sub remove_caps{
+   my ($self, $board, $caps) = @_;
+   for my $cap (@$caps){
+      for my $adj ($self->get_adjacents($board,$cap)){
+         $self->remove_stone $adj
+      }
+      $self->remove_stone $cap
+   }
+}
+
+#__PACKAGE__->meta->apply ($rulemap);
+
+
