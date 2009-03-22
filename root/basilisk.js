ae10483ca7566562c346a39c01fd4b66d6aa23c7
Zach Morgan <zpmorgan@gmail.com>
2009-03-22 04:25:24 -0400

    edge-wrap scrolling works

--- root/basilisk.js
+++ root/basilisk.js
@@ -1,11 +1,41 @@
 
 //Scrolling:
 
+//globals:
+//var offset_ew = 0;
+//var offset_ns = 0;
+var w;
+var h;
+
+
 function scroll (direction){
-   alert("blep");
+   var board = document.getElementById('board');
+   if (direction=='up'){
+      //send top row to bottom
+      var row = board.rows[h];
+      row.parentNode.insertBefore (row, board.rows[1]);
+   }
+   else if (direction=='down'){
+      var row = board.rows[1];
+      row.parentNode.insertBefore (row, board.rows[h+1]);
+   }
+   else{
+	   for(var i = 0; i < h+2; i++){
+	      var row = board.rows[i];
+	      if (direction=='left'){
+	         var cell = row.cells[1];
+	         cell.parentNode.insertBefore(cell, row.cells[w+1]);
+         }
+         else{ //right
+            var cell = row.cells[w];
+            cell.parentNode.insertBefore (cell, row.cells[1]);
+         }
+      }
+	}
+   //msgbox = document.getElementById("msg");
+   //msgbox.innerHTML = "blep";
 }
 
-
-
+function drawPlease (){} //for canvas?
 
 
