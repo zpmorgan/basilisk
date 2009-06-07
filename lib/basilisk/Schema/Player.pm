545118f3a54082a73b1acc8662a5b704899296ca
zach morgan <zach@zach-netbook.(none)>
2009-06-07 17:11:36 -0400

    partly unbreaking status by searching user`s games with an omniquery

--- lib/basilisk/Schema/Player.pm
+++ lib/basilisk/Schema/Player.pm
@@ -2,7 +2,7 @@ package basilisk::Schema::Player;
 use warnings;
 use strict;
 use basilisk::Constants qw/GAME_RUNNING/;
-use List::MoreUtils qw(uniq);
+use List::MoreUtils qw(uniq any indexes);
 
 use base qw/DBIx::Class/;
 #use Glicko2;
@@ -47,61 +47,84 @@ sub update_rating{
 #return game id, your side...,
 #opponents in game, with sides,
 #Time of last move is important for both, I think.
-sub games_to_move {
-   my $self = shift;
+sub games_to_move{
+   my ($self, $all_turnwise) = shift;
    my $name = $self->name;
    
    my $omniquery = $self->search_related ('player_to_game', 
    {
       status => GAME_RUNNING,
-      #'game.phase' => {'==' => 'player_to_game.entity'}, #wrong
+      #ignore if phase != me.entity's phases
    },
    {
-      join => ['player', {'game' => 'ruleset'} ],
-      select => ['player.name', 
-            'me.entity', 'me.gid', 'me.pid',
-            'game.phase as phase', 
-            'ruleset.phase_description as pd'
-            ],
+      join => {'game' => [{'player_to_game' => 'player'}, 'ruleset']},
+      select => [
+         'player.name', 'player.id', 
+         'me.entity', 'me.gid',
+         'phase', #from game
+         'phase_description', #from ruleset
+         #'player_2.id AS p2id','player_2.name AS p2name',
+      ],
+      as => [qw/name pid entity   gid phase pd/],
    });
-   
-   #die $self->player_to_game->next->game->ruleset;
-   
-   my %games;
    #die ${$omniquery->as_query};
+   #die $omniquery->next->get_columns;
+   my %games;
    for my $game ($omniquery->all()){
       my $gid = $game->gid;
-      next if $games{$gid}++;
+      next if $games{$gid};
       #die $game->get_columns;
       $games{$gid} = {
          #row => $game,
          id => $gid,
          pd => $game->get_column('pd'),
          phase => $game->get_column('phase'),
+         players => {},
       };
    }
    for my $p2g ($omniquery->all()){
-      my $gid = $p2g->get_column('gid');
+      my $gid = $p2g->gid;
+      next unless $games{$gid};
+      
       my $pid = $p2g->get_column('pid');
       my $pname = $p2g->get_column('name');
-  #    my $pd = $p2g->get_column('pd');
       my $entity = $p2g->get_column('entity');
-      $games{$pid}->{opponents}->{pid} = {
+      my ($phase,$pd) = @{{$p2g->get_columns}}{qw/phase pd/};
+      die ($phase,$pd);
+      my @sides = sides_of_entity ($pd, $entity);
+      my @phases = phases_of_entity ($pd, $entity);
+      
+      unless ($pid == $self->id or $all_turnwise){
+         #delete game info from %games, if it's someone else's turn
+         if (any {$_ == $phase} @phases){
+            delete $games{$gid};
+            next;
+         }
+      }
+      
+      $games{$gid}->{players}->{$pid} = {
          id => $pid,
          name => $pname,
-         entity => $p2g->entity,
-    #     sides => sides_of_entity ($pd, $entity),
+         entity => $entity,
+         sides => \@sides,
       };
    }
    #die join ',',map {$_->{id}} @games;
    return values %games;
 }
 
+
 sub sides_of_entity{ 
    my ($pd, $ent) = @_;
    #my $pd = $self->phase_description;
    my @sides =  $pd =~ /$ent([bwr])/g; 
-   return [uniq @sides];
+   return uniq @sides;
+}
+sub phases_of_entity{ 
+   my ($pd, $ent) = @_;
+   #my $pd = $self->phase_description;
+   my @phases = split ' ', $pd;
+   return indexes {$_ =~ /$ent/} @phases;
 }
 
 1;
